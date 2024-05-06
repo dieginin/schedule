fb011bfaff6dbfd570826fd069c042e028d6a192
dieginin <diegobalestra9@gmail.com>
2024-05-06 02:33:06 -0400

    update manage_view.py

--- views/manage_view.py
+++ views/manage_view.py
@@ -1,4 +1,6 @@
-from flet import Page, View
+from flet import Divider, MainAxisAlignment, Page, Row, View
+
+from components import CenteredColumn, HomeBtn, IconBtn, Subtitle, Title
 
 
 class ManageView(View):
@@ -7,4 +9,30 @@ class ManageView(View):
         self.page: Page = page
 
         self.route = "/manage"
-        self.controls = []
+
+        self.floating_action_button = HomeBtn(visible=False)
+        self.controls = [self.__store(), self.__members()]
+
+    def __title_section(self, value: str | None = None) -> tuple:
+        return Title(value), Divider()
+
+    def __store(self) -> CenteredColumn:
+        return CenteredColumn(
+            [
+                *self.__title_section("Store"),
+                Row(
+                    [
+                        Subtitle(f"{self.page.client_storage.get('store_initials')}"),
+                        IconBtn("edit", tooltip="Edit name"),
+                    ],
+                    alignment=MainAxisAlignment.CENTER,
+                ),
+            ]
+        )
+
+    def __members(self) -> CenteredColumn:
+        return CenteredColumn(
+            [
+                *self.__title_section("Members"),
+            ]
+        )
