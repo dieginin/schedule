47bb98a6da405817df78e4747bc5431487b59aed
dieginin <diegobalestra9@gmail.com>
2024-05-06 00:31:15 -0400

    update router.py

--- services/router.py
+++ services/router.py
@@ -14,14 +14,14 @@ class Router:
     def __init__(self, page: Page):
         self.page: Page = page
 
-        self.page.on_route_change = self.on_route_change
+        self.page.on_route_change = self.__on_route_change
 
-        self.initial_route()
+        self.__initial_route()
 
-    def initial_route(self):
+    def __initial_route(self):
         self.page.go("/")
 
-    def on_route_change(self, e: RouteChangeEvent):
+    def __on_route_change(self, e: RouteChangeEvent):
         self.page.views.clear()
         if self.page.client_storage.get("store_initials"):
             self.page.views.append(routes[e.route](self.page))
