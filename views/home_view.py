394dcbc9d63c8195d0ed7c23b5b39729c170876d
dieginin <diegobalestra9@gmail.com>
2024-05-06 00:17:14 -0400

    update home_view.py

--- views/home_view.py
+++ views/home_view.py
@@ -1,10 +1,50 @@
-from flet import Page, View
+from flet import CrossAxisAlignment, Image, MainAxisAlignment, Page, View
+
+from components import (
+    CenteredColumn,
+    PrimaryButton,
+    SecondaryButton,
+    TertiaryButton,
+    Title,
+)
 
 
 class HomeView(View):
     def __init__(self, page: Page):
         super().__init__()
-        self.page = page
+        self.page: Page = page
 
         self.route = "/"
-        self.controls = []
+
+        self.vertical_alignment = MainAxisAlignment.SPACE_EVENLY
+        self.horizontal_alignment = CrossAxisAlignment.CENTER
+
+        self.controls = [
+            CenteredColumn(
+                [
+                    Image("logo.png", width=500),
+                    Title(
+                        f"Welcome to {self.page.client_storage.get('store_initials')}!"
+                    ),
+                ]
+            ),
+            CenteredColumn(
+                [
+                    PrimaryButton(
+                        "Set Schedule",
+                        icon="calendar_month_rounded",
+                        on_click=lambda _: self.page.go("/set"),
+                    ),
+                    TertiaryButton(
+                        "Export Schedule",
+                        icon="download_rounded",
+                        on_click=lambda _: self.page.go("/export"),
+                    ),
+                    SecondaryButton(
+                        "Manage Store",
+                        icon="settings_rounded",
+                        on_click=lambda _: self.page.go("/manage"),
+                    ),
+                ]
+            ),
+        ]
