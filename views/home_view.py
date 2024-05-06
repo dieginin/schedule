c69cb867bbefff23db7696e07d18773715638b6e
dieginin <diegobalestra9@gmail.com>
2024-05-06 00:31:18 -0400

    update home_view.py

--- views/home_view.py
+++ views/home_view.py
@@ -19,32 +19,33 @@ class HomeView(View):
         self.vertical_alignment = MainAxisAlignment.SPACE_EVENLY
         self.horizontal_alignment = CrossAxisAlignment.CENTER
 
-        self.controls = [
-            CenteredColumn(
-                [
-                    Image("logo.png", width=500),
-                    Title(
-                        f"Welcome to {self.page.client_storage.get('store_initials')}!"
-                    ),
-                ]
-            ),
-            CenteredColumn(
-                [
-                    PrimaryButton(
-                        "Set Schedule",
-                        icon="calendar_month_rounded",
-                        on_click=lambda _: self.page.go("/set"),
-                    ),
-                    TertiaryButton(
-                        "Export Schedule",
-                        icon="download_rounded",
-                        on_click=lambda _: self.page.go("/export"),
-                    ),
-                    SecondaryButton(
-                        "Manage Store",
-                        icon="settings_rounded",
-                        on_click=lambda _: self.page.go("/manage"),
-                    ),
-                ]
-            ),
-        ]
+        self.controls = [self.__title(), self.__buttons()]
+
+    def __title(self) -> CenteredColumn:
+        return CenteredColumn(
+            [
+                Image("logo.png", width=500),
+                Title(f"Welcome to {self.page.client_storage.get('store_initials')}!"),
+            ]
+        )
+
+    def __buttons(self) -> CenteredColumn:
+        return CenteredColumn(
+            [
+                PrimaryButton(
+                    "Set Schedule",
+                    icon="calendar_month_rounded",
+                    on_click=lambda _: self.page.go("/set"),
+                ),
+                TertiaryButton(
+                    "Export Schedule",
+                    icon="download_rounded",
+                    on_click=lambda _: self.page.go("/export"),
+                ),
+                SecondaryButton(
+                    "Manage Store",
+                    icon="settings_rounded",
+                    on_click=lambda _: self.page.go("/manage"),
+                ),
+            ]
+        )
