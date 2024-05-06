cacf26f7512551884aa0aa10130c6d71cb9ed50a
dieginin <diegobalestra9@gmail.com>
2024-05-06 03:07:30 -0400

    update layout.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,3 @@
 from .buttons import HomeBtn, IconBtn, PrimaryBtn, SecondaryBtn, TertiaryBtn
-from .layout import CenteredColumn
+from .layout import CenteredColumn, CenteredRow, Section
 from .text import Subtitle, Title
--- components/layout.py
+++ components/layout.py
@@ -1,4 +1,6 @@
-from flet import Column, Control, CrossAxisAlignment
+from flet import Column, Control, CrossAxisAlignment, Divider, MainAxisAlignment, Row
+
+from .text import Title
 
 
 class CenteredColumn(Column):
@@ -6,3 +8,22 @@ class CenteredColumn(Column):
         super().__init__()
         self.controls = controls
         self.horizontal_alignment = CrossAxisAlignment.CENTER
+
+
+class CenteredRow(Row):
+    def __init__(self, controls: list[Control] | None = None):
+        super().__init__()
+        self.controls = controls
+        self.alignment = MainAxisAlignment.CENTER
+
+
+class Section(CenteredColumn):
+    def __init__(self, title: str, control: Control | None = None):
+        super().__init__()
+        self.controls = self.__controls(title, control)
+
+    def __controls(self, title: str, control: Control | None = None) -> list[Control]:
+        controls = [Title(title), Divider()]
+        if control:
+            controls.append(control)
+        return controls
