21a9247d6288cff8a1efdbbfbcc8fe9c110cfbbb
dieginin <diegobalestra9@gmail.com>
2024-05-12 00:40:01 -0400

    update layout.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,3 @@
 from .buttons import HomeBtn, IconBtn, PrimaryBtn, SecondaryBtn, TertiaryBtn
-from .layout import CenteredColumn, CenteredRow, Section
+from .layout import CenteredColumn, CenteredRow
 from .text import Subtitle, Title
--- components/layout.py
+++ components/layout.py
@@ -1,29 +1,15 @@
-from flet import Column, Control, CrossAxisAlignment, Divider, MainAxisAlignment, Row
-
-from .text import Title
+import flet as ft
 
 
-class CenteredColumn(Column):
-    def __init__(self, controls: list[Control] | None = None):
+class CenteredColumn(ft.Column):
+    def __init__(self, controls: list[ft.Control] | None = None):
         super().__init__()
         self.controls = controls
-        self.horizontal_alignment = CrossAxisAlignment.CENTER
+        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
 
-class CenteredRow(Row):
-    def __init__(self, controls: list[Control] | None = None):
+class CenteredRow(ft.Row):
+    def __init__(self, controls: list[ft.Control] | None = None):
         super().__init__()
         self.controls = controls
-        self.alignment = MainAxisAlignment.CENTER
-
-
-class Section(CenteredColumn):
-    def __init__(self, title: str, control: Control | None = None):
-        super().__init__()
-        self.controls = self.__controls(title, control)
-
-    def __controls(self, title: str, control: Control | None = None) -> list[Control]:
-        controls = [Title(title), Divider()]
-        if control:
-            controls.append(control)
-        return controls
+        self.alignment = ft.MainAxisAlignment.CENTER
