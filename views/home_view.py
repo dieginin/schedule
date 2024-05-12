23ad2b796df4aab17b2c71493c926694f9cb4fa4
dieginin <diegobalestra9@gmail.com>
2024-05-12 00:40:24 -0400

    update home_view.py

--- views/home_view.py
+++ views/home_view.py
@@ -1,24 +1,24 @@
-from flet import CrossAxisAlignment, Image, MainAxisAlignment, Page, View
+import flet as ft
 
 from components import CenteredColumn, PrimaryBtn, SecondaryBtn, TertiaryBtn, Title
 
 
-class HomeView(View):
-    def __init__(self, page: Page):
+class HomeView(ft.View):
+    def __init__(self, page: ft.Page):
         super().__init__()
-        self.page: Page = page
+        self.page: ft.Page = page
 
         self.route = "/"
 
-        self.vertical_alignment = MainAxisAlignment.SPACE_EVENLY
-        self.horizontal_alignment = CrossAxisAlignment.CENTER
+        self.vertical_alignment = ft.MainAxisAlignment.SPACE_EVENLY
+        self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
         self.controls = [self.__title(), self.__buttons()]
 
     def __title(self) -> CenteredColumn:
         return CenteredColumn(
             [
-                Image("logo.png", width=500),
+                ft.Image("logo.png", width=500),
                 Title(f"Welcome to {self.page.client_storage.get('store_initials')}!"),
             ]
         )
