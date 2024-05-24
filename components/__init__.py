d97eb36cdfa575e0644c4fa98c94172423eb253e
dieginin <diegobalestra9@gmail.com>
2024-05-24 17:25:49 -0400

    update layout.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,3 +1,3 @@
 from .buttons import HomeBtn, IconBtn, PrimaryBtn, SecondaryBtn, TertiaryBtn
-from .layout import CenteredColumn, CenteredRow
+from .layout import CenteredColumn, CenteredRow, Field
 from .text import Subtitle, Title
--- components/layout.py
+++ components/layout.py
@@ -1,4 +1,32 @@
+from typing import Literal
+
 import flet as ft
+
+
+class Field(ft.TextField):
+    def __init__(
+        self,
+        hint_text: str | None = None,
+        max_length: int | None = None,
+        on_submit=None,
+        on_change=None,
+        capitalization: Literal["word", "upper"] | None = None,
+        width: ft.OptionalNumber = None,
+    ):
+        super().__init__()
+        self.hint_text = hint_text
+        self.max_length = max_length
+        self.counter_text = " "
+        self.on_submit = on_submit
+        self.on_change = on_change
+        self.capitalization = (
+            ft.TextCapitalization.CHARACTERS
+            if capitalization == "upper"
+            else ft.TextCapitalization.WORDS if capitalization == "word" else None
+        )  # type: ignore
+        self.width = width
+        self.border = ft.InputBorder.UNDERLINE
+        self.text_align = ft.TextAlign.CENTER
 
 
 class CenteredColumn(ft.Column):
